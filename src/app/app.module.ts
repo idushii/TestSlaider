63003f82954d72be9b93cc9344e8a8ddf93dadc2
Maria <you@example.com>
2020-03-28 00:01:30 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SliderComponent } from './slider/slider.component';
+import { GuideComponent } from './guide/guide.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      SliderComponent
+      SliderComponent,
+      GuideComponent
    ],
    imports: [
       BrowserModule,
